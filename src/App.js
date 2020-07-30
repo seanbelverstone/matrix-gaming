3409c17f9813205969364d85ed325f00fb5e76dc
Sean Belverstone <seanbelverstone@gmail.com>
2020-07-29 20:45:38 -0500

    Had to comment out the pages for the App to work, as it was returning weirdly as theres nothing in each page! Ill have to make sure to flesh them out before uncommenting them. Added bootstrap, changed the serviceworker and also included the CDN for bootstrap css files in index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,36 +10,27 @@ import MatrixAcademy from "./pages/MatrixAcademy";
 import Subscribe from "./pages/Subscribe";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
-
+import TopNav from "./components/TopNav";
 import './App.css';
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/tournaments">
-            <Tournaments />
-          </Route>
-          <Route path="/matrixAcademy">
-            <MatrixAcademy />
-          </Route>
-          <Route path="/subscribe">
-            <Subscribe />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <TopNav />
+          {/* <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/tournaments" component={Tournaments} />
+            <Route path="/matrixAcademy"component={MatrixAcademy} />
+            <Route path="/subscribe" component={Subscribe} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/contact" component={Contact} />
+          </Switch> */}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
